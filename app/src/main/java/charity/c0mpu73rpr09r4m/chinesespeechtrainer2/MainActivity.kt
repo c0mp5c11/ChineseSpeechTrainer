764dde38ad266c68b297f118d0f5543734d19f5c
Daniel Ohm <daniel.edward.ohm.jr@gmail.com>
2025-11-21 19:19:28 -0800

    styled

--- app/src/main/java/charity/c0mpu73rpr09r4m/chinesespeechtrainer2/MainActivity.kt
+++ app/src/main/java/charity/c0mpu73rpr09r4m/chinesespeechtrainer2/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,6 +16,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,7 +41,7 @@ class MainActivity : ComponentActivity() {
                         startActivity(intent)
                     }
                     else {
-                        updateText("${dictionaryEntry?.englishWord}\n\n${dictionaryEntry?.chineseWord}\n\n${dictionaryEntry?.pinyin}\n")
+                        updateText("${dictionaryEntry?.englishWord}\n${dictionaryEntry?.chineseWord}\n${dictionaryEntry?.pinyin}")
                     }
 
                     Box(
@@ -48,12 +51,28 @@ class MainActivity : ComponentActivity() {
                             .padding(16.dp),
                         contentAlignment = Alignment.Center
                     ) {
-                        Text(
-                            text = displayText,
-                            fontSize = 32.sp,
-                            color = Color.White,
-                            textAlign = TextAlign.Center
-                        )
+                        Box(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .fillMaxSize()
+                                .padding(16.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .background(Color.White, shape = RectangleShape)
+                                    .border(1.dp, Color.White, RectangleShape)
+                                    .padding(70.dp, 20.dp)
+                            ) {
+                                Text(
+                                    text = displayText,
+                                    fontSize = 40.sp,
+                                    color = Color.Black,
+                                    textAlign = TextAlign.Center,
+                                    lineHeight = 50.sp
+                                )
+                            }
+                        }
                     }
                 }
             }
